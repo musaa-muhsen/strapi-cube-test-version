d94ae51bda437f79aabf799344dc1ed6080db756
musaa-muhsen <musaamuhsen@gmail.com>
2021-11-26 12:34:42 +0000

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '980f299cb2701a24e2ee03868447d442'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0188e1c8-c1e3-4cd2-b7a5-9e70524de086'
+};
